b747f26a24e5b336874102c3be36e46a733f83f3
Jack <xueyi.mr@hotmail.com>
2016-03-28 15:32:48 +0800

    Text React-Native
    this is Text

---
+++ ReactNative-Hybird-Demo/ReactNative-Hybird-Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReactNative-Hybird-Demo
+//
+//  Created by Jack on 16/3/16.
+//  Copyright © 2016年 Jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ReactNative-Hybird-Demo/ReactNative-Hybird-Demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ReactNative-Hybird-Demo
+//
+//  Created by Jack on 16/3/16.
+//  Copyright © 2016年 Jack. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
